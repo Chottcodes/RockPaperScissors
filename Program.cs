a51468e0b43177def23b82f75a6f11bdc30aa124
Chottcodes <cwellshott@gmail.com>
2025-01-15 14:40:55 -0800

    Update

--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<RockPaperServices>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+    policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
---
+++ README.md
@@ -0,0 +1,13 @@
+// Your name 
+Chris Wells-Hott
+
+ // Date Revised: 
+ 1/15/2025
+
+ // Exercise or Lab name 
+ Rock,Paper,Scissors,Lizard,Spock
+
+ // Brief Description of what you did. 
+ Create a Endpoint that give a response from the CPU 
+
+// Person who Peer Reviewed your assignment
